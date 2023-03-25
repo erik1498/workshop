5a52c38a685169b75d2d1110114d5e0bb68ced19
erickhene <djefrisonferdinan@gmail>
2023-03-25 11:29:38 +0800

    Add Post

--- be/index.js
+++ be/index.js
@@ -2,7 +2,11 @@ const express =  require("express")
 const app = express()
 
 app.get("/", (req, res) => {
-    
+    res.send("OKE")
+})
+
+app.post("/", (req, res) => {
+    console.log(req.body)
 })
 
 app.listen(3000, () => {
